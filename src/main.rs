ed55e0ea11a7bb715a7ea641d0fcafca4fdd4a84
Fabien D <fabien.ducret@hotmail.fr>
2024-02-01 17:47:55 +0100

    [FEATURE] Use of match for asking name

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
 use std::{error::Error, io, process};
 
 fn main() {
-    let name = ask_for_name().unwrap_or_else(|err| {
-        println!("Problem asking for name: {}", err);
-        process::exit(1);
-    });
-
-    hello_rust::say_hello_to(&name);
+    match ask_for_name() {
+        Err(err) => exit_process_with(err),
+        Ok(name) => hello_rust::say_hello_to(&name),
+    }
 }
 
 fn ask_for_name() -> Result<String, Box<dyn Error>> {
@@ -16,4 +14,9 @@ fn ask_for_name() -> Result<String, Box<dyn Error>> {
     io::stdin().read_line(&mut name)?;
 
     Ok(String::from(name.trim()))
+}
+
+fn exit_process_with(err: Box<dyn Error>) {
+    println!("{}", err);
+    process::exit(1);
 }
